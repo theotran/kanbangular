c5d321211a2999f775b42e6b3b8be7b2d00d446c
theo tran <theotran@rocketmail.com>
2016-03-09 21:41:45 -1000

    successfully made user authentication used passport, session, etc

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 var express = require('express');
 var app = express();
 var db = require('./models');
-
 var bodyParser = require('body-parser');
+var path = require('path');//
+
+//requiring for authentication/passport/etc...
+var CONFIG = require('./server-config.json');//required for sessions
+var passport = require('passport');
+var localStrategy = require('passport-local');
+var session = require('express-session');
+
 
 app.set('view engine', 'jade');//Tell Express which Template engine we are using by NPM module name
 app.set('views', 'views');//tell express where our template files live
@@ -21,9 +28,67 @@ app.use(express.static(__dirname + '/public'));
 // app.use(require('method-override'));
 
 
+//configurations for cookies/sessions
+app.use(session(CONFIG.SESSION));//configuring sessions
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+//looking in our Users database for the user!
+passport.use(new localStrategy(
+  function (username, password, done) {
+    db.User.find({
+      where: {
+        username: username
+      }
+    }).
+    then(function (user) {
+      //if no user found
+      if (!user) {//not authenticated
+        
+        return done(null, false);
+      }
+      //if user is found
+      if (user.password === password) {
+        console.log("hooray!");
+        return done(null, user);//authenticated
+      }
+    });
+}));
+
+passport.serializeUser(function (user, done) {//putting an id on our object
+  console.log(user);
+  return done(null, user.id);
+});
+
+passport.deserializeUser(function (id, done) {//grabbing the id on our object
+  console.log('your id is :', id);
+  db.User.findById(id)
+    .then(function (user) {
+      return done(null, user);
+    });
+});
+
+function isAuthenticated (req, res, next) {
+  if (!req.isAuthenticated()) {
+    return  res.redirect('/login');
+  }
+  return next();
+}
+
+
+
 app.get('/login', function (req, res) {
   res.render('login');
 });
+
+
+app.post('/login',
+  passport.authenticate('local', {
+    successRedirect : '/',
+    failureRedirect : '/login'
+  })
+);
 
 //posting new 'cards' to our database!!!
 app.post('/api', function (req, res) {
@@ -103,6 +168,12 @@ app.put('/api/cards/:id/edit', function (req, res) {
       return res.json(card);
     });
 });
+
+
+app.get('/', isAuthenticated, function (req, res) {
+  res.sendFile(path.resolve('./public/kanban.html'));//makes the path more absolute, helps get the filename etc...
+});
+
 
 //finding all cards from our database
 app.get('/api', function (req, res) {
